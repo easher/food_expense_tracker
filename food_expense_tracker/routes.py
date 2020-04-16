a4bef574fdca490112efa46a30724b99287e6611
Jackson Maslow <jackson.maslow@gmail.com>
2020-04-15 20:43:43 -0400

    (WIP) add JWT auth

--- food_expense_tracker/routes.py
+++ food_expense_tracker/routes.py
@@ -7,6 +7,48 @@ from food_expense_tracker.models import User, Account
 
 
 from flask import request, jsonify
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
+
+# Setup the Flask-JWT-Extended extension
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+jwt = JWTManager(app)
+
+@app.route('/api/login', methods = ['POST'])
+def login():
+    json_request_data = request.get_json()
+    schema = {
+        'username': { 'type': 'string', 'required': True, 'empty': False},
+        'password': {'type': 'string', 'required': True, 'empty': False}
+    }
+    
+    validator = Validator(schema)
+    isValidRequest = validator.validate(request.json)
+    if not isValidRequest:
+        return jsonify(validator.errors)
+    
+    password = json_request_data.get('password')
+    username = json_request_data.get('username')
+    # check that password matches username
+    # if not, return 400
+    
+    if password != 'password':
+        return jsonify({"msg": "incorrect password"}), 400
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token), 200
+
+
+# Protect a view with jwt_required, which requires a valid access token
+# in the request to access.
+@app.route('/protected', methods=['GET'])
+@jwt_required
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 
 
 @app.route('/api/users', methods = ['POST'])
